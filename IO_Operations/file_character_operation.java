5ceaa6c3c185ed00a7cf4379e495c7cf73f77d9f
Asᴍᴀ P A <120763637+asmapa@users.noreply.github.com>
2023-11-10 10:40:12 +0530

    Update file_character_operation.java

--- IO_Operations/file_character_operation.java
+++ IO_Operations/file_character_operation.java
@@ -1,22 +1,39 @@
+
 package IO_Operations;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class file_character_operation {
-    public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("example.txt"))) {
+    public static void main(String[] args) throws IOException{
+        
+        try  {
+            FileReader fil=new FileReader("example.txt");
+            BufferedReader reader = new BufferedReader(fil);
+            FileWriter fir=new FileWriter("example2.txt");
+            BufferedWriter writer=new BufferedWriter(fir);
             String line;
-            int lineNumber = 1;
+           // int lineNumber = 1;
 
             while ((line = reader.readLine()) != null) {
-                System.out.printf("%d: %s%n", lineNumber, line);
-                lineNumber++;
+                writer.write(line);
+                writer.newLine();
+               
+                
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            fil.close();
+                reader.close();
+                writer.close();
+                fir.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("The file cant see");
         }
     }
 }
+
+
 
